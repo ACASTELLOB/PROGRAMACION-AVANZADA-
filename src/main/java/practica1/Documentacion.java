81ae8a04d9956eb49d168931f50c3b56ec76dd57
Raul <al394830@uji.es>
2021-03-16 11:07:24 +0100

    Añadida atributos de la clase Persona

---
+++ src/main/java/practica1/Documentacion.java
@@ -0,0 +1,7 @@
+package practica1;
+
+public class Documentacion extends Resultado {
+    String formato;
+    int numPaginas;
+    int espacioDisco;
+}
---
+++ src/main/java/practica1/Programa.java
@@ -0,0 +1,7 @@
+package practica1;
+
+public class Programa extends Resultado{
+    String lenguaje;
+    int numLineasCodigo;
+    int numModulos;
+}
