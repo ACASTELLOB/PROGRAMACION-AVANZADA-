d05506cf1911a4c6d60e1660bad16404c9d43d3f
ACASTELLOB <tonicasteb@gmail.com>
2021-03-23 10:09:28 +0100

    GestionDeProyectos, añadimos el case 2

--- src/main/java/practica1/GestionDeProyectos.java
+++ src/main/java/practica1/GestionDeProyectos.java
@@ -1,6 +1,7 @@
 package practica1;
 
 import java.util.Scanner;
+import java.util.Date;
 public class GestionDeProyectos {
 
 
@@ -11,13 +12,12 @@ public class GestionDeProyectos {
         System.out.println("============            MENU        =================");
         System.out.println("=====================================================");
         System.out.println("0. Salir");
-        System.out.println("1.Crear proyecto");
-        System.out.println("2.Dar de alta a las personas que trabajan en el proyecto");
-        System.out.println("3.Dar de alta las tareas con sus datos");
-        System.out.println("4.Marcar una tarea como finalizada");
-        System.out.println("5.Introducir o eliminar una persona de una tarea");
-        System.out.println("6.Listar las personas asignadas a un proyecto");
-        System.out.println("7.Listar las tareas de un proyecto.");
+        System.out.println("1.Dar de alta a las personas que trabajan en el proyecto");
+        System.out.println("2.Dar de alta las tareas con sus datos");
+        System.out.println("3.Marcar una tarea como finalizada");
+        System.out.println("4.Introducir o eliminar una persona de una tarea");
+        System.out.println("5.Listar las personas asignadas a un proyecto");
+        System.out.println("6.Listar las tareas de un proyecto.");
         do {
             System.out.print("\nElige una opcion (0..7): ");
             opcion = teclado.nextInt();
@@ -29,6 +29,9 @@ public class GestionDeProyectos {
 
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce el nombre del proyecto: ");
+        String titulo = teclado.nextLine();
+        Proyecto proyecto = new Proyecto(titulo);
         int opcion;
         do {
             opcion = menu(teclado);
@@ -37,10 +40,34 @@ public class GestionDeProyectos {
                     System.out.println(" Cerramos el gestor");
                     break;
                 case 1:
-                    System.out.print("Introduce el nombre del proyecto: ");
-                    String titulo = teclado.nextLine();
-                    Proyecto proyecto = new Proyecto(titulo);
+                    System.out.println("Introduce el nombre de la persona: ");
+                    String nombre = teclado.nextLine();
+                    System.out.println("Introduce el correo de la persona: ");
+                    String correo = teclado.nextLine();
+                    Persona persona = new Persona(nombre, correo);
+                    proyecto.añadirPersona(persona);
+                    break;
                 case 2:
+                    Date fecha= new Date();
+                    System.out.print("Introduce el título de la tarea");
+                    String tituloTarea =teclado.nextLine();
+                    System.out.println("Introduce el tipo de resultado: D para documentación, W para web, P para programa: ");
+                    String tipoResultado= teclado.nextLine();
+                    if(tipoResultado.equals("D")){
+                        Documentacion documento =new Documentacion();
+                        Tarea tarea =new Tarea(tituloTarea,fecha.getDay(),documento );
+                    }
+                    else if(tipoResultado.equals("W")){
+                        PagWeb web = new PagWeb();
+                        Tarea tarea =new Tarea(tituloTarea,fecha.getDay(), web);
+                    }
+                    else{
+                        Programa prog = new Programa();
+                        Tarea tarea =new Tarea(tituloTarea,fecha.getDay(), prog);
+                    }
+
+
+
 
 
 
