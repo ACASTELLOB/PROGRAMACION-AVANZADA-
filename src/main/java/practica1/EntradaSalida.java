69d1d350275a704357de56fb7787458ea1804eba
ACASTELLOB <tonicasteb@gmail.com>
2021-03-26 17:04:27 +0100

    EntradaSalida añadido

--- src/main/java/practica1/EntradaSalida.java
+++ src/main/java/practica1/EntradaSalida.java
@@ -4,10 +4,30 @@ import java.util.Scanner;
 
 public class EntradaSalida {
 
-    Scanner teclado;
+    Scanner  teclado;
 
     public EntradaSalida(){
-        teclado = new new Scanner(System.in);
+        teclado = new Scanner(System.in);
+    }
+    public int menu() {
+        int opcion;
+        System.out.println("\n\n");
+        System.out.println("=====================================================");
+        System.out.println("============            MENU        =================");
+        System.out.println("=====================================================");
+        System.out.println("0. Salir");
+        System.out.println("1.Dar de alta a las personas que trabajan en el proyecto");
+        System.out.println("2.Dar de alta las tareas con sus datos");
+        System.out.println("3.Marcar una tarea como finalizada");
+        System.out.println("4.Introducir o eliminar una persona de una tarea");
+        System.out.println("5.Listar las personas asignadas a un proyecto");
+        System.out.println("6.Listar las tareas de un proyecto.");
+        do {
+            System.out.print("\nElige una opcion (0..6): ");
+            opcion = teclado.nextInt();
+        } while ( (opcion<0) || (opcion>7) );
+        teclado.nextLine(); // Elimina retorno de carro del buffer de entrada
+        return opcion;
     }
 
     public String leer(){
--- src/main/java/practica1/GestionDeProyectos.java
+++ src/main/java/practica1/GestionDeProyectos.java
@@ -5,37 +5,15 @@ import java.util.Date;
 public class GestionDeProyectos {
 
 
-    public static int menu(Scanner teclado) {
-        int opcion;
-        System.out.println("\n\n");
-        System.out.println("=====================================================");
-        System.out.println("============            MENU        =================");
-        System.out.println("=====================================================");
-        System.out.println("0. Salir");
-        System.out.println("1.Dar de alta a las personas que trabajan en el proyecto");
-        System.out.println("2.Dar de alta las tareas con sus datos");
-        System.out.println("3.Marcar una tarea como finalizada");
-        System.out.println("4.Introducir o eliminar una persona de una tarea");
-        System.out.println("5.Listar las personas asignadas a un proyecto");
-        System.out.println("6.Listar las tareas de un proyecto.");
-        do {
-            System.out.print("\nElige una opcion (0..6): ");
-            opcion = teclado.nextInt();
-        } while ( (opcion<0) || (opcion>7) );
-        teclado.nextLine(); // Elimina retorno de carro del buffer de entrada
-        return opcion;
-    }
-
-
     public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in);
+
         EntradaSalida EntSal = new EntradaSalida();
         System.out.print("Introduce el nombre del proyecto: ");
         String titulo = EntSal.leer();
         Proyecto proyecto = new Proyecto(titulo);
         int opcion;
         do {
-            opcion = menu(teclado);
+            opcion = EntSal.menu();
             switch (opcion) {
                 case 0:
                     EntSal.mostrar("Cerramos el gestor");
