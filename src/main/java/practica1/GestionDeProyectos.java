5a32dd59d52c87b351fd6ffa2e2bfe29035605c1
ACASTELLOB <tonicasteb@gmail.com>
2021-03-29 16:33:47 +0200

    finished

--- src/main/java/practica1/GestionDeProyectos.java
+++ src/main/java/practica1/GestionDeProyectos.java
@@ -4,98 +4,132 @@ import java.util.Scanner;
 import java.util.Date;
 public class GestionDeProyectos {
 
+    public static int menu(Scanner teclado) {
+        int opcion;
+        System.out.println("\n\n");
+        System.out.println("=====================================================");
+        System.out.println("============            MENU        =================");
+        System.out.println("=====================================================");
+        System.out.println("0. Salir");
+        System.out.println("1.Dar de alta a las personas que trabajan en el proyecto");
+        System.out.println("2.Dar de alta las tareas con sus datos");
+        System.out.println("3.Marcar una tarea como finalizada");
+        System.out.println("4.Introducir o eliminar una persona de una tarea");
+        System.out.println("5.Listar las personas asignadas a un proyecto");
+        System.out.println("6.Listar las tareas de un proyecto.");
+        do {
+            System.out.print("\nElige una opcion (0..6): ");
+            opcion = teclado.nextInt();
+        } while ( (opcion<0) || (opcion>7) );
+        teclado.nextLine(); // Elimina retorno de carro del buffer de entrada
+        return opcion;
+    }
+
 
     public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
 
-        EntradaSalida EntSal = new EntradaSalida();
         System.out.print("Introduce el nombre del proyecto: ");
-        String titulo = EntSal.leer();
+        String titulo = teclado.nextLine();
         Proyecto proyecto = new Proyecto(titulo);
         int opcion;
         do {
-            opcion = EntSal.menu();
+            opcion = menu(teclado);
             switch (opcion) {
                 case 0:
-                    EntSal.mostrar("Cerramos el gestor");
+                    System.out.println("Cerramos el gestor");
                     break;
                 case 1:
-                    EntSal.mostrar("Introduce el nombre de la persona");
-                    String nombre = EntSal.leer();
-                    EntSal.mostrar("Introduce el correo de la persona: ");
-                    String correo = EntSal.leer();
+                    System.out.println("Introduce el nombre de la persona");
+                    String nombre = teclado.nextLine();
+                    System.out.println("Introduce el correo de la persona: ");
+                    String correo = teclado.nextLine();
                     Persona persona = new Persona(nombre, correo);
                     proyecto.añadirPersona(persona);
                     break;
                 case 2:
-                    Date fecha= new Date();
-                    EntSal.mostrar("Introduce el título de la tarea");
-                    String tituloTarea =EntSal.leer();
-                    EntSal.mostrar("Introduce el tipo de resultado: D para documentación, W para web, P para programa: ");
-                    String tipoResultado= EntSal.leer();
-                    EntSal.mostrar("Introduce la prioridad de la tarea");
-                    int prioridad=EntSal.leerInt();
+                    System.out.println("Introduce el título de la tarea");
+                    String tituloTarea =teclado.nextLine();
+                    System.out.println("Introduce el tipo de resultado: D para documentación, W para web, P para programa: ");
+                    String tipoResultado= teclado.nextLine();
+                    System.out.println("Introduce la prioridad de la tarea");
+                    int prioridad=Integer.parseInt(teclado.nextLine());
+                    System.out.println("Introduce el identificador del documento");
+                    String ident=teclado.nextLine();
+                    System.out.println("Introduce las horas");
+                    int horas=Integer.parseInt(teclado.nextLine());
+                    System.out.println("Quieres que sea interno el resultado si es que sí, escribe S, sino escribe N");
+                    String interno=teclado.nextLine();
+                    boolean esInterno=false;
+                    if (interno.equals("S")){
+                        esInterno=true;
+                    }
+
                     if(tipoResultado.equals("D")){
 
-
-                        EntSal.mostrar("Introduce el identificador del documento");
-                        String ident=EntSal.leer();
-                        EntSal.mostrar("Introduce el formato del documento");
-                        String format= EntSal.leer();
-                        EntSal.mostrar("Introduce las horas");
-                        int horas=EntSal.leerInt();
-                        EntSal.mostrar("Introduce el número de páginas");
-                        int pag= EntSal.leerInt();
-                        EntSal.mostrar("Introduce el espacio en disco para tu documento");
-                        int esp=EntSal.leerInt();
-                        EntSal.mostrar("Quieres que sea interno el resultado si es que sí, escribe S, sino escribe N");
-                        String interno=EntSal.leer();
-                        boolean esInterno=false;
-                        if (interno.equals("S")){
-                            esInterno=true;
-                        }
-
+                        System.out.println("Introduce el formato del resultado");
+                        String format= teclado.nextLine();
+                        System.out.println("Introduce el número de páginas del resultado");
+                        int pag= Integer.parseInt(teclado.nextLine());
+                        System.out.println("Introduce el espacio en disco para tu resultado");
+                        int esp=Integer.parseInt(teclado.nextLine());
                         Documentacion documento =new Documentacion(ident,horas,esInterno,format,pag,esp );
                         Tarea tarea =new Tarea(tituloTarea,documento, prioridad);
                         proyecto.añadirTarea(tarea);
                     }
                     else if(tipoResultado.equals("W")){
-                        //hola
-                        PagWeb web = new PagWeb();
+                        System.out.println("Introduce el lenguaje de la web");
+                        String lenguaje= teclado.nextLine();
+                        System.out.println("Introduce el backend de la web");
+                        String backend= teclado.nextLine();
+                        System.out.println("Quieres que sea estática la web si es que sí, escribe S, sino escribe N");
+                        String estatica=teclado.nextLine();
+                        boolean esEstatica=false;
+                        if (estatica.equals("S")){
+                            esEstatica=true;
+                        }
+                        PagWeb web = new PagWeb(ident, horas, esInterno,  esEstatica,lenguaje,backend);
                         Tarea tarea =new Tarea(tituloTarea,web,prioridad);
                         proyecto.añadirTarea(tarea);
 
                     }
                     else {
-                        Programa prog = new Programa();
+                        System.out.println("Introduce el lenguaje del programa");
+                        String lenguaje= teclado.nextLine();
+                        System.out.println("Introduce el número el lineas de código");
+                        int numLineasCodigo= Integer.parseInt(teclado.nextLine());
+                        System.out.println("Introduce el número de módulos");
+                        int numModulos=Integer.parseInt(teclado.nextLine());
+                        Programa prog = new Programa(ident, horas, esInterno, lenguaje,numLineasCodigo, numModulos);
                         Tarea tarea = new Tarea(tituloTarea,  prog, prioridad);
                         proyecto.añadirTarea(tarea);
                     }
                     break;
                 case 3:
-                    EntSal.mostrar("Introduce el título de la tarea:");
-                    String Finalizada= EntSal.leer();
+                    System.out.println("Introduce el título de la tarea:");
+                    String Finalizada= teclado.nextLine();
                     Boolean hecha = proyecto.finalizarTarea(Finalizada);
                     if (hecha){
-                        EntSal.mostrar("Tarea Finalizada");
+                        System.out.println("Tarea Finalizada");
                     }
                     else{
-                        EntSal.mostrar("No se encuentra el titulo de la tarea");
+                        System.out.println("No se encuentra el titulo de la tarea");
                     }
                     break;
                 case 4:
-                    EntSal.mostrar("Introudce el nombre de la persona");
-                    String nombrePersona= EntSal.leer();
-                    EntSal.mostrar("Introduce el titulo de la tarea donde la quieras añadir o eliminar:");
-                    String tituloProyecto= EntSal.leer();
-                    EntSal.mostrar(proyecto.añadirEliminarPersona(nombrePersona,tituloProyecto));
+                    System.out.println("Introudce el nombre de la persona");
+                    String nombrePersona= teclado.nextLine();
+                    System.out.println("Introduce el titulo de la tarea donde la quieras añadir o eliminar:");
+                    String tituloProyecto= teclado.nextLine();
+                    System.out.println(proyecto.añadirEliminarPersona(nombrePersona,tituloProyecto));
                     break;
                 case 5:
                     for(Persona elem:proyecto.personas){
-                        EntSal.mostrar(elem.toString());
+                        System.out.println(elem.toString());
                     }
                 case 6:
                     for(Tarea elem:proyecto.tareas){
-                        EntSal.mostrar(elem.toString());
+                        System.out.println(elem.toString());
                     }
 
             }
