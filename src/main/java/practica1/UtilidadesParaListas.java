dea873651ed02dccf5c250c332c7ea797e35644d
Raul <al394830@uji.es>
2021-03-30 12:12:26 +0200

    funciona de una vez

--- src/main/java/practica1/UtilidadesParaListas.java
+++ src/main/java/practica1/UtilidadesParaListas.java
@@ -14,4 +14,12 @@ public class UtilidadesParaListas<E>{
         return res;
     }
 
+    public static <T,E extends tieneClave<T>> Boolean claveUnica(E elem, tieneLista<E> objetoConLista) {
+        for (E aux : objetoConLista.getLista()) {
+            if (aux.getClave().equals(elem.getClave())) {
+                return false;
+            }
+        }
+        return  true;
+    }
 }
